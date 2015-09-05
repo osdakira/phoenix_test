2ad6f3562dfb24f334f6ef81714e9b3d603af8b1
Akira Osada <osd.akira@gmail.com>
2015-09-06 08:55:02 +0900

    mix phoenix.new phoenix_test/

---
+++ test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixTest.PageControllerTest do
+  use PhoenixTest.ConnCase
+
+  test "GET /" do
+    conn = get conn(), "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
